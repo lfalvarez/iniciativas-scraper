2cccb6fb0bebf5f471a305edef6ae9cd6f80a380
Felipe Álvarez <felipe.alvarez@thoughtworks.com>
2022-01-15 16:17:10 -0300

    Creando punto de entrada para el scraper

--- scraper.py
+++ scraper.py
@@ -4,7 +4,11 @@ from iniciativas.spiders.iniciativas_list_spider import IniciativasListScrapy
 
 process = CrawlerProcess(settings={
     "FEEDS": {
-        "file.csv": {"format": "csv"},
+        "file.csv": {
+            "format": "csv",
+            "fields": ['id', 'apoyos', 'titulo', 'categoria', 'problema', 'propuesta_articulado', 'que_debe_contemplar',
+                       'situacion_ideal', 'url']
+        },
     },
 })
 
